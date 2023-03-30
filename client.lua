fdce6170c6524d2e334564d32ec5fcabf04566bc
neux0 <69310876+neux0@users.noreply.github.com>
2023-03-30 21:33:35 +0300

    Add files via upload

---
+++ client.lua
@@ -0,0 +1,144 @@
+﻿--www.atomgaming.net | by neux#8720
+
+local screenW, screenH = guiGetScreenSize()
+local iftarVakti
+local fullMetin
+local gettok = gettok
+local panel = {
+    button = {},
+    window = {},
+    combobox = {},
+    label = {}
+}
+addEvent( "iftarVakti:PanelAc", true )
+addEvent( "İftarVakti:VeriGeldi", true )
+
+function PaneliAc()
+	guiSetVisible( panel.window[1], true )
+	showCursor(true)
+end	
+addEventHandler( "iftarVakti:PanelAc", root, PaneliAc )
+
+function VeriGeldi(vakitS)
+    iftarVakti = vakitS
+    if iftarVakti == nil or type(iftarVakti) ~= "table" then return outputChatBox("[!] #ffffffİftar sayacı siteden veri çekemiyor. Lütfen Kurucuya bunu bildir.", 255, 0, 0, true) end 
+    addEventHandler("onClientRender", root, SayacGoster)
+end	
+addEventHandler( "İftarVakti:VeriGeldi", root, VeriGeldi )
+
+addEventHandler("onClientResourceStart", resourceRoot,
+    function()
+        panel.window[1] = guiCreateWindow((screenW - 240) / 2, (screenH - 200) / 2, 240, 200, "Atom Gaming İftar Sayacı Sistemi", false)
+        panel.combobox[1] = guiCreateComboBox(10, 30, 220, 200, "Şehir seç", false, panel.window[1])
+        panel.button[2] = guiCreateButton(10, 170, 220, 20, "Kaydet", false, panel.window[1])
+        panel.label[1] = guiCreateLabel(10, 80, 220, 60, "Mini haritanın altında, iftara kaç\nsaat kaldığını bu sistem\nsayesinde göreceksin.\nAllah kabul etsin. ~neux", false, panel.window[1])
+        guiLabelSetHorizontalAlign(panel.label[1],"center")
+        guiSetFont(panel.label[1], "clear-normal")
+        guiSetVisible(panel.window[1],false)
+        abo = ""
+        for i=1, #sehirler do
+            guiComboBoxAddItem(panel.combobox[1], sehirler[i])
+        end
+    end
+)
+
+addEventHandler('onClientGUIClick', resourceRoot, 
+    function() 
+        if (source == panel.button[2]) then
+            secili = guiComboBoxGetSelected(panel.combobox[1])
+            sehir = guiComboBoxGetItemText(panel.combobox[1], secili)
+            if secili == -1 then return print("Lütfen bir şehir seçiniz.") end
+            triggerServerEvent("İftarVakti:VeriGetir", localPlayer, sehir)
+            guiSetVisible( panel.window[1], false )
+	        showCursor(false)
+        end 
+    end 
+)
+
+function SayacGoster()
+    if not iftarVakti then return end
+    SahurIftarHesapla()
+    dxDrawText (fullMetin, 24, screenH - 41, screenW, screenH, tocolor ( 0, 0, 0, 255 ), 1.3, "clear-normal" ) --golge
+    dxDrawText (fullMetin, 24, screenH - 43, screenW, screenH, tocolor ( 255, 255, 255, 255 ), 1.3, "clear-normal" ) --yazi
+end
+
+function SahurIftarHesapla()
+    local bugunT, yarinT  = TarihCek()
+    local tarih = getRealTime()
+    local gun = 86400
+    local saatDakika = tarih.hour*3600+tarih.minute*60+tarih.second
+    local sahurBugun = strToMinHour(iftarVakti[bugunT][1])
+    local sahurYarin = strToMinHour(iftarVakti[yarinT][1])
+    local iftar = strToMinHour(iftarVakti[bugunT][2])
+    local iftarKalan = iftar-saatDakika
+    if iftarKalan<0 then iftarKalan=iftarKalan+gun end
+    local sahurKalan = (saatDakika>sahurBugun and sahurYarin or sahurBugun)-saatDakika
+    if sahurKalan<0 then sahurKalan=sahurKalan+gun end
+    local guncelKalan = iftarKalan<sahurKalan and iftarKalan or sahurKalan
+    local metin = iftarKalan<sahurKalan and "İftara " or "Sahurun bitimine "
+    fullMetin = metin..""..totime(guncelKalan)
+end
+
+function totime(timestamp)
+    kalanim = ""
+	local timestamp = timestamp - (math.floor(timestamp/86400) * 86400)
+	local hours = math.floor(timestamp/3600)
+	timestamp = timestamp - (math.floor(timestamp/3600) * 3600)
+	local mins = math.floor(timestamp/60)
+	local secs = timestamp - (math.floor(timestamp/60) * 60)
+    if hours > 0 then 
+        kalanim = hours.." saat "
+    end
+    if mins > 0 then 
+        kalanim = kalanim..mins.." dakika "
+    end      
+    if secs > 0 then 
+        kalanim = kalanim..secs.." saniye"
+    end       
+	return kalanim
+end
+
+function strToMinHour(str)
+    return gettok(str,1,":")*3600+gettok(str,2,":")*60
+end
+
+function TarihCek()
+    local t = os.time()
+    local bugunT = os.date("%Y-%m-%d",t)
+    local yarinT = os.date("%Y-%m-%d",t + 86400)              
+    return bugunT, yarinT
+end
+
+addCommandHandler("iftar", function(command, parametre)
+    if parametre == "ac" then
+    if isEventHandlerAdded( 'onClientRender', root, SayacGoster) then return outputChatBox("[!] #ffffffİftar sayacı zaten açık.", 255, 0, 0, true) end
+        addEventHandler("onClientRender", root, SayacGoster)
+        outputChatBox("[!] #ffffffİftar sayacı açıldı.", 0, 255, 0, true) 
+    elseif parametre == "kapat" then
+        removeEventHandler("onClientRender", root, SayacGoster)
+        outputChatBox("[!] #ffffffİftar sayacı kapatıldı.", 0, 255, 0, true) 
+    elseif parametre == "degistir" then 
+        if isEventHandlerAdded( 'onClientRender', root, SayacGoster) then
+            removeEventHandler("onClientRender", root, SayacGoster)      
+        end  
+        guiSetVisible( panel.window[1], true )
+        showCursor(true)
+    else
+        outputChatBox("[!] #ffffffLütfen geçerli bir parametre gir. (ac-kapat-degistir)", 255, 0, 0, true)
+    end
+end, false)
+
+
+function isEventHandlerAdded( sEventName, pElementAttachedTo, func )
+    if type( sEventName ) == 'string' and isElement( pElementAttachedTo ) and type( func ) == 'function' then
+        local aAttachedFunctions = getEventHandlers( sEventName, pElementAttachedTo )
+        if type( aAttachedFunctions ) == 'table' and #aAttachedFunctions > 0 then
+            for i, v in ipairs( aAttachedFunctions ) do
+                if v == func then
+                    return true
+                end
+            end
+        end
+    end
+    return false
+end
---
+++ server.lua
@@ -0,0 +1,56 @@
+﻿-- iftar sayacı by neux#8720
+
+function ScriptBaslayinca()
+    for k, player in ipairs(getElementsByType("player")) do
+        hesap = getPlayerAccount(player)
+        if not isGuestAccount(hesap) then
+            BilgiCek(player)      
+        end
+    end
+end
+addEventHandler("onResourceStart", resourceRoot, ScriptBaslayinca)
+
+addEvent("İftarVakti:VeriGetir", true)
+addEventHandler("İftarVakti:VeriGetir", root, function( sehirAdi)
+    bugunT, yarinT, yarininYariniT = TarihCek()
+    sehir = sehirAdi
+    setAccountData(getPlayerAccount(client), "yasadigiSehir", sehir)
+    fetchRemote ( "https://namaz-vakti.vercel.app/api/timesFromPlace?country=Turkey&region="..sehir.."&city="..sehir.."&date="..bugunT.."&days=3&timezoneOffset=180", callBack, "", false, client, bugunT, yarinT, yarininYariniT)
+end)
+
+addEventHandler("onPlayerLogin", root, function(_, hesap)
+    BilgiCek(source)
+end)
+
+function callBack(responseData, errorCode, playerToReceive, bugunT, yarinT, yarininYariniT)
+    if errorCode == 0 then
+        tablem = fromJSON(responseData)
+        vakit = {
+            [""..bugunT..""] = {tablem["times"][bugunT][1],tablem["times"][bugunT][5]},
+            [""..yarinT..""] = {tablem["times"][yarinT][1],tablem["times"][yarinT][5]},
+            [""..yarininYariniT..""] = {tablem["times"][yarininYariniT][1],tablem["times"][yarininYariniT][5]},
+        }
+    else
+        vakit = "hata"
+    end
+    triggerClientEvent (playerToReceive, "İftarVakti:VeriGeldi", playerToReceive, vakit)
+end
+--www.atomgaming.net
+function BilgiCek(oyuncu)
+    data = getAccountData(getPlayerAccount(oyuncu), "yasadigiSehir")
+    if data then 
+        bugunT, yarinT, yarininYariniT = TarihCek()
+        sehir = data
+        fetchRemote ( "https://namaz-vakti.vercel.app/api/timesFromPlace?country=Turkey&region="..sehir.."&city="..sehir.."&date="..bugunT.."&days=3&timezoneOffset=180", callBack, "", false, oyuncu, bugunT, yarinT, yarininYariniT)
+    else
+        triggerClientEvent(oyuncu, "iftarVakti:PanelAc", oyuncu)
+    end
+end
+
+function TarihCek()
+    local t = os.time()
+    local bugunT = os.date("%Y-%m-%d",t)
+    local yarinT = os.date("%Y-%m-%d",t + 86400) 
+    local yarininYariniT = os.date("%Y-%m-%d",t + 172800)        
+    return bugunT, yarinT, yarininYariniT
+end
